9076f105cef2fd8b03df4031c1efce5ade62e995
nancynasser24102003-coder <nancynasser24102003@gmail.com>
2026-07-20 15:29:34 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,29 +1,39 @@
+
 import cv2
-import numpy as np ,math
-img=np.zeros((500,500),dtype=np.uint8)
+import numpy as np,math
+
+img_1=np.zeros((500,500),dtype=np.uint8)
+for i in range (50,301):
+    img_1[i,i]=255
 
 
-
-for x in range (0,499):
-    y=x+1
-    img[y,x]=255
-
-
-
+Cx,Cy=100,100
 r=50
-Cx,Cy=350,150
+for x in range (Cx-r,Cx+r+1):
+    y_square=r**2-(x-Cx)**2
+    if y_square >=0:
+        y_offest=math.sqrt(y_square)
 
-for X in range (Cx-r,Cx+r+1):
-    Y_Square=r**2-(X-Cx)**2
-    if  Y_Square >=0:
-      Y_Offset=math.sqrt(Y_Square)
-      Y1=int(Cy+Y_Offset)
-      Y2=int(Cy-Y_Offset)
-      img[X,Y1]=255
-      img[X,Y2]=255
-cv2.imshow("Image",img)
+    y1=int(Cy-y_offest)
+    y2=int(Cy+y_offest)
+    img_1[y1,x]=255
+    img_1[y2,x]=255
+
+cv2.imshow("image1",img_1)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+img_2=img_1=np.zeros((500,500),dtype=np.uint8)
+cv2.line(img_2,(50,50),(300,300),[255,255,255],1)
+cv2.circle(img_2,(100,100),50,[255,255,255],1)
+cv2.imshow("image2",img_2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
 
 
 
