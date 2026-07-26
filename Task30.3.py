ace141e7fd3a7313e75e90670e8060a81a1622c5
nancynasser24102003-coder <nancynasser24102003@gmail.com>
2026-07-26 11:04:36 +0300

    Update Task30.3.py

--- Task30.3.py
+++ Task30.3.py
@@ -1,26 +1,32 @@
 import cv2
 import numpy as np 
 import matplotlib.pyplot as plt
-import os 
-
-img=cv2.imread(r"C:\Users\Etijah\Desktop\659349064_18576944251041430_2870406156677966472_n-1.jpg")
-img_SizeinHard=os.path.getsize(r"C:\Users\Etijah\Desktop\659349064_18576944251041430_2870406156677966472_n-1.jpg")
-print(f"The Size of Image in Hard :{img_SizeinHard}")
-img_SizeinMemory=img.nbytes
-print(f"The Size of Image in Memory : {img_SizeinMemory}")
-Ratio_SizeinMemorytoHard=img_SizeinMemory/img_SizeinHard
-print(f"The Ratio Between The Size of Image in Memory to in Hard:{Ratio_SizeinHardtoMemory}")
-
-if Ratio_SizeinMemorytoHard>1:
-    print("The Size of Image in Memory > The Size of Image in Hard")
-elif Ratio_SizeinMemorytoHard==1:
-    print("The Size of Image in Memory = The Size of Image in Hard")
-else:
-    print("The Size of Image in Memory < The Size of Image in Hard")
 
 
-if img_SizeinMemory > img_SizeinHard:
-    print("Memory size is larger than file size because the image is decompressed when loaded into RAM.")
+
+arr_uint8=np.array([250,245,4],dtype=np.uint8)
+arr_uint8=arr_uint8+15
+print(arr_uint8)
+
+
+
+arr_uint8=np.array([250,245,4],dtype=np.uint8)
+arr_float32=arr_uint8.astype(np.float32)
+arr_float32=arr_float32+15.0
+print(arr_float32)
+
+
+if arr_float32.max() > 255:
+    print("float32 stores values greater than 255 without overflow.")
+if arr_uint8.max() <255:
+    print("uint8 wraps around when values exceed 255, causing overflow.")
+
+
+
+
+
+
+
 
 
 
